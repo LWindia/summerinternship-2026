21602923875d5a46b6c669179a729142d012466c
auspicious27 <sayeedpersonal27@gmail.com>
2026-01-09 12:43:11 +0530

    Fix: Make footer image responsive for mobile view

--- src/components/Footer3.tsx
+++ src/components/Footer3.tsx
@@ -5,20 +5,17 @@ const Footer3 = ()=>{
 
     return (
         <>
-        <div className="w-full  h-full mx-auto ">
-
-
+        <div className="w-full h-auto mx-auto relative">
                 <Image 
-                    className="w-full object-cover" 
+                    className="w-full h-auto object-cover" 
                     src="/assets/footerImage.jpg" 
                     alt="Footer"
                     width={1920}
                     height={400}
+                    sizes="100vw"
                     loading="lazy"
+                    priority={false}
                 />
-       
-
-            
         </div>
         
         </>
